dfeba8f151117153066038913b41b22eb32e026f
unknown <jamesbolakok@gmail.com>
2026-03-09 12:22:25 +0530

    All files committed successfully

---
+++ script.js
@@ -0,0 +1,16 @@
+function revealOnScroll() {
+  const reveals = document.querySelectorAll(".reveal, .reveal-left, .reveal-right");
+
+  reveals.forEach((el) => {
+    const windowHeight = window.innerHeight;
+    const elementTop = el.getBoundingClientRect().top;
+    const revealPoint = 100;
+
+    if (elementTop < windowHeight - revealPoint) {
+      el.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+revealOnScroll();
